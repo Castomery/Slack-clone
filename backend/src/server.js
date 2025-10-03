ccfb4a0d18d7a272c17a5acc8cb2795a33b96934
Castomery <vladpoligalov3@gmail.com>
2025-10-03 16:40:39 +0300

    stream and sentry added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,3 +1,4 @@
+import "../instrument.mjs";
 import express from 'express';
 import "dotenv/config";
 import cors from "cors";
@@ -6,6 +7,8 @@ import { connectDB } from './configs/mongoDB.js';
 import {clerkMiddleware} from "@clerk/express";
 import { functions, inngest } from './configs/inngest.js';
 import { serve } from "inngest/express";
+import chatRouter from './routes/chat.route.js';
+import * as Sentry from "@sentry/node";
 
 const app = express();
 
@@ -13,9 +16,17 @@ app.use(clerkMiddleware());
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/inngest", serve({ client: inngest, functions }));
-
 app.get('/', (req,res) => res.send("Api is working"));
+
+app.get("/debug-sentry", (req,  res) => {
+    throw new Error("sentry error");
+})
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/chat", chatRouter);
+
+Sentry.setupExpressErrorHandler(app);
+
 
 const startServer = async() =>{
     try {
