ece194a7dc37080c78fa2bb0ae27d465cedcd5a8
Castomery <vladpoligalov3@gmail.com>
2025-10-05 18:06:33 +0300

    invite functionality added

--- frontend/src/components/CustomChannelPreview.jsx
+++ frontend/src/components/CustomChannelPreview.jsx
@@ -2,13 +2,13 @@ import { HashIcon } from "lucide-react";
 import React from "react";
 
 const CustomChannelPreview = ({ channel, setActiveChannel, activeChannel }) => {
+
   const isActive = activeChannel && activeChannel.id === channel.id;
-  const isDM =
-    channel.data.member_count === 2 && channel.data.id.includes("user_");
+  const isDM =channel.data.member_count === 2 && channel.data.id.includes("user_");
 
   if (isDM) return null;
 
-  const unreadCount = channel.uneadCount;
+  const unreadCount = channel.countUnread();
 
   return (
     <button
